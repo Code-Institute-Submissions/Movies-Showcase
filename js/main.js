f41080d0c9147639985b65ceeabd7d72c30ee07e
Yaoma <a.o.kalugin@gmail.com>
2019-10-18 19:39:53 +0100

    Started making onlick event for poster to see futher movie details.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+// Constants.
 const BASE_URL = "https://api.themoviedb.org/3/movie/";
 const API_KEY = "75577ee1d6c6894588c8299ecbef6d44";
 const POSTER_BASE_URL = "https://image.tmdb.org/t/p/";
 const POSTER_SIZE = "w185/";
 
+// All document elements we will be interacting with.
 const movieDisplay = document.getElementById("moviesDisplay");
 const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
@@ -12,20 +14,27 @@ const topRated = document.getElementById("topRated");
 const upcoming = document.getElementById("upcoming");
 const header = document.getElementById("header");
 
+// Default variables.
 let filter = "popular";
 let page = "1";
 let totalPages = 0;
 let isSortingVisible = false;
 
-// String building function which returns a valid url.
-let urlBuilder = (BASE_URL, filter, API_KEY, page) => `${BASE_URL}${filter}?api_key=${API_KEY}&page=${page}`;
-// String building function which returns a valid url for poster.
+// String building function for single movie url.
+let movieUrlBuilder = (BASE_URL, filter, API_KEY) => `${BASE_URL}${filter}?api_key=${API_KEY}`;
+// String building function for movie list url.
+let moviesUrlBuilder = (BASE_URL, filter, API_KEY, page) => `${BASE_URL}${filter}?api_key=${API_KEY}&page=${page}`;
+// String building function for poster url.
 let posterUrlBuilder = (POSTER_BASE_URL, POSTER_SIZE, poster_path) => `${POSTER_BASE_URL}${POSTER_SIZE}${poster_path}`;
 
-// Asynchronous function for fetching data from given url.
-async function getMovies(BASE_URL, filter, API_KEY, page) {
+async function getMovie(id) {
+    console.log(id);
+}
+
+// Fetching movie list.
+async function getMovies(filter, page) {
     // Fetching data from given url.
-    const RESPONSE = await fetch(urlBuilder(BASE_URL, filter, API_KEY, page));
+    const RESPONSE = await fetch(moviesUrlBuilder(BASE_URL, filter, API_KEY, page));
     // Transforming data into json format.
     const DATA = await RESPONSE.json();
     // Getting number of total pages.
@@ -41,6 +50,49 @@ async function getMovies(BASE_URL, filter, API_KEY, page) {
         poster.style.backgroundImage = `url("${posterUrl}")`;
         // Appending poster to our display section.
         movieDisplay.appendChild(poster);
+
+
+        poster.addEventListener("click", async function getMovie() {
+            movieDisplay.innerHTML = "";
+            const response = await fetch(movieUrlBuilder(BASE_URL, element.id, API_KEY));
+
+            const data = await response.json();
+
+            let movieDetails = document.createElement("div");
+            let backdrop = document.createElement("div");
+            let moviePoster = document.createElement("div");
+            let title = document.createElement("div");
+            let releaseDate = document.createElement("div");
+            let rating = document.createElement("div");
+            let overviewCaption = document.createElement("div");
+            let overview = document.createElement("div");
+
+            movieDetails.id = "movieDetails";
+            backdrop.id = "backdrop";
+            moviePoster.id = "poster";
+            title.id = "title";
+            releaseDate.id = "releaseDate";
+            rating.id = "rating";
+            overviewCaption.id = "overviewCaption";
+            overview.id = "overview";
+
+            moviePoster.style.backgroundImage = `url("${posterUrl}")`;
+            title.innerHTML = data.title;
+            releaseDate.innerHTML = data.release_date;
+            rating.innerHTML = data.vote_average;
+            overviewCaption.innerHTML = "Overview";
+            overview.innerHTML = data.overview;
+
+            movieDetails.appendChild(backdrop);
+            movieDetails.appendChild(moviePoster);
+            movieDetails.appendChild(title);
+            movieDetails.appendChild(releaseDate);
+            movieDetails.appendChild(rating);
+            movieDetails.appendChild(overviewCaption);
+            movieDetails.appendChild(overview);
+
+            movieDisplay.appendChild(movieDetails);
+        });
     });
 }
 
@@ -49,7 +101,7 @@ let changeMovies = () => {
     // Clearing movie display section from previous page data.
     movieDisplay.innerHTML = "";
     // Fetching new data with updated page value.
-    getMovies(BASE_URL, filter, API_KEY, page);
+    getMovies(filter, page);
     // Scrolling to the top of the screen.
     window.scrollTo(0, 0);
 }
@@ -108,15 +160,18 @@ upcoming.addEventListener("click", () => {
 
 // Sorting menu animation
 sortButton.addEventListener("click", () => {
+    // Check if sort menu is open.
     if (isSortingVisible) {
+        // Change header height.
         header.style.height = "50px";
         isSortingVisible = false;
     } else {
+        // Change header height.
         header.style.height = "100px";
         isSortingVisible = true;
     }
 });
 
 // Loading data on window load.
-window.onload = getMovies(BASE_URL, filter, API_KEY, page);
+window.onload = getMovies(filter, page);
 
