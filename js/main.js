fc4c1bc4dcbad7c37f3f8c0de6046b6bb4d1e4af
Yaoma <a.o.kalugin@gmail.com>
2019-10-18 16:19:13 +0100

    Added next and previous button functions.

--- js/main.js
+++ js/main.js
@@ -6,7 +6,11 @@ const POSTER_SIZE = "w185/";
 let filter = "popular";
 let page = "1";
 
+let TOTAL_PAGES = 0;
+
 const movieDisplay = document.getElementById("moviesDisplay");
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
 
 // String building function which returns a valid url.
 let urlBuilder = (BASE_URL, filter, API_KEY, page) => `${BASE_URL}${filter}?api_key=${API_KEY}&page=${page}`;
@@ -19,6 +23,8 @@ async function getMovies(BASE_URL, filter, API_KEY, page) {
     const RESPONSE = await fetch(urlBuilder(BASE_URL, filter, API_KEY, page));
     // Transforming data into json format.
     const DATA = await RESPONSE.json();
+    // Getting number of total pages.
+    TOTAL_PAGES = DATA.total_pages;
     // Looping through data results.
     DATA.results.forEach(element => {
         // Getting single poster url.
@@ -33,6 +39,36 @@ async function getMovies(BASE_URL, filter, API_KEY, page) {
     });
 }
 
-// Testing Section
-getMovies(BASE_URL, filter, API_KEY, page);
+// Go to next page.
+nextButton.addEventListener("click", () => {
+    // Check if we are not on last page.
+    if (page != TOTAL_PAGES) {
+        // Increase page value.
+        page++;
+        // Clearing movie display section from previous page data.
+        movieDisplay.innerHTML = "";
+        // Fetching new data with updated page value.
+        getMovies(BASE_URL, filter, API_KEY, page);
+        // Scrolling to the top of the screen.
+        window.scrollTo(0,0);
+    }
+});
+
+// Go to previous page.
+previousButton.addEventListener("click", () => {
+    // Check if we are not on first page.
+    if (page != 1) {
+        // Decreasing page value.
+        page--;
+        // Clearing movie display section from previous page data.
+        movieDisplay.innerHTML = "";
+        // Fetching new data with updated page value.
+        getMovies(BASE_URL, filter, API_KEY, page);
+        // Scrolling to the top of the screen.
+        window.scrollTo(0,0);
+    }
+});
+
+// Loading data on window load.
+window.onload = getMovies(BASE_URL, filter, API_KEY, page);
 
